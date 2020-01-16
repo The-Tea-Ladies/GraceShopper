12b59beb8c91d5245b6fcd4321643b43a53099c6
anastasiia burlya <anastasiia.burlya@gmail.com>
2020-01-16 13:06:40 -0500

    feature: security route done  for delete user

--- server/api/users.js
+++ server/api/users.js
@@ -1,6 +1,19 @@
 const router = require('express').Router()
+
+const HttpError = require('../utils/HttpError')
 const {User} = require('../db/models')
 module.exports = router
+
+router.param('id', (req, res, next, id) => {
+  User.findByPk(id)
+    .then(user => {
+      if (!user) throw HttpError(404)
+      req.requestedUser = user
+      next()
+      return null
+    })
+    .catch(next)
+})
 
 router.get('/', async (req, res, next) => {
   try {
@@ -11,6 +24,35 @@ router.get('/', async (req, res, next) => {
       attributes: ['id', 'email']
     })
     res.json(users)
+  } catch (err) {
+    next(err)
+  }
+})
+
+// router.get('/:id', async (req, res, next) => {
+//   try {
+//     const user = await User.findByPk(req.params.id)
+//     res.json(user)
+
+//   } catch (err) {
+//     next(err)
+//   }
+// })
+
+const adminsOnly = (req, res, next) => {
+  if (!req.params.isAdmin) {
+    const err = new Error('Not allowed!')
+    err.status = 401
+    return next(err)
+  }
+  next()
+}
+
+router.delete('/:id', adminsOnly, async (req, res, next) => {
+  try {
+    const id = req.params.id
+    await User.destroy({where: {id}})
+    res.sendStatus(204)
   } catch (err) {
     next(err)
   }
