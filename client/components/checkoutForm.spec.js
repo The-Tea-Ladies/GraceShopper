107bd607eaee00700265fe4c5867d77dfe293dbc
anastasiia burlya <anastasiia.burlya@gmail.com>
2020-01-21 18:18:55 -0500

    feature: specs in progress

---
+++ client/components/checkoutForm.spec.js
@@ -0,0 +1,34 @@
+/* global describe beforeEach it */
+
+import {expect} from 'chai'
+import React from 'react'
+import enzyme, {shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import CheckoutForm from './checkoutForm'
+import Testing from './testing'
+
+const adapter = new Adapter()
+enzyme.configure({adapter})
+
+describe('Testing form', () => {
+  let form
+
+  beforeEach(() => {
+    form = shallow(
+      <CheckoutForm
+        state={{
+          shippingname: '',
+          shippingaddress: '',
+          billingname: '',
+          billingaddress: ''
+        }}
+        handleSubmit={() => null}
+        handleClick={() => null}
+      />
+    )
+  })
+
+  it('renders in a h4', () => {
+    expect(form.find('h3').text()).to.be.equal('Welcome')
+  })
+})
