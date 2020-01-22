d44f8f8f7072447f81fbcb9971dbfe03eeaa49a9
anastasiia burlya <anastasiia.burlya@gmail.com>
2020-01-22 10:48:00 -0500

    shopping cart icon added

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
-// import {Icon, InlineIcon} from '@iconify/react'
-// import shoppingCart from '@iconify/icons-typcn/shopping-cart'
+import {Icon} from 'react-icons-kit'
+import {shopping_cart} from 'react-icons-kit/ikons/shopping_cart'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
@@ -17,8 +17,12 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           {/* The navbar will show these links after you log in */}
 
           <Link to="/cart">
-            {/* <Icon icon={shoppingCart} className="icon" color="black" /> */}
-            Shopping Cart
+            <Icon
+              size="32"
+              icon={shopping_cart}
+              className="icon"
+              color="black"
+            />
           </Link>
           <Link to="/profile">Profile</Link>
           <Link to="/home">Home</Link>
@@ -31,8 +35,12 @@ const Navbar = ({handleClick, isLoggedIn}) => (
           {/* The navbar will show these links before you log in */}
 
           <Link to="/cart">
-            {/* <Icon icon={shoppingCart} className="icon" color="black" /> */}
-            Shopping Cart
+            <Icon
+              size="32"
+              icon={shopping_cart}
+              className="icon"
+              color="black"
+            />
           </Link>
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
